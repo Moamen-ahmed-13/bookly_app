f624db9161f9198777fbf2240fe4aa40b9d07699
Moamen-ahmed-13 <mmnmessed1@gmail.com>
2024-12-03 14:10:39 +0200

    "Refactor SplashViewBody to StatefulWidget, add sliding text animation and navigation to HomeView, update constants and main theme"

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,35 @@
+import 'package:bookly_app/Features/splash/presentation/views/Home/presentation/views/home_view.dart';
+import 'package:bookly_app/Features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:bookly_app/consatants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +37,34 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        
         Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 4,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+    slidingAnimation.addListener(() {
+      setState(() {});
+    });
+  }
+
+    void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() =>const HomeView(),transition: Transition.fade,duration: kTransitionDuration);
+    });
+  }
+
 }
--- lib/consatants.dart
+++ lib/consatants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor=Color.fromARGB(255, 27, 15, 58);
+const kPrimaryColor = Color.fromARGB(255, 27, 15, 58);
+const kTransitionDuration = Duration(microseconds: 250);
