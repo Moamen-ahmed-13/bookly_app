e027ae39ea99735b7f560e07a5b8a2baf2d6df3e
Moamen-ahmed-13 <mmnmessed1@gmail.com>
2024-12-08 13:41:21 +0200

    `BookDetailsViewBody` updated

--- lib/Features/splash/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Features/splash/presentation/views/Home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_app/Features/splash/presentation/views/Home/presentation/views/widgets/custum_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -6,14 +7,18 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 23.0),
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 23.0),
       child: Column(
         children: [
-          CustomBookDetailsAppBar(),
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.only(left: width * .2, right: 30),
+            child: const FeaturedListViewItem(),
+          ),
         ],
       ),
     );
   }
 }
-
