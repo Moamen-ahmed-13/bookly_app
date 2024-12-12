7664ee6cc494a9806c5f398637deb2019534aa00
Moamen-ahmed-13 <mmnmessed1@gmail.com>
2024-12-12 16:02:37 +0200

    `Added  method to NewestBooksCubitCubit`

--- lib/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit_cubit.dart
+++ lib/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_books_cubit_state.dart';
 
 class NewestBooksCubitCubit extends Cubit<NewestBooksCubitState> {
-  NewestBooksCubitCubit() : super(NewestBooksCubitInitial());
+  NewestBooksCubitCubit(this.homeRepo) : super(NewestBooksCubitInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewestBooksCubitLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) => emit(
+        NewestBooksCubitFailure(failure.errorMsg),
+      ),
+      (success) => emit(
+        NewestBooksCubitSuccess(success),
+      ),
+    );
+  }
 }
